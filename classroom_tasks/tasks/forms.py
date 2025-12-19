51c4c7e41a48322cf56fb503bde2c31342f03c5e
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-19 11:46:07 +0100

    Creación del formulario TaskForm para validación de tareas

---
+++ classroom_tasks/tasks/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'type', 'members', 
+                  'requires_teacher_validation', 'due_date']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        task_type = cleaned_data.get('type')
+        members = cleaned_data.get('members')
+
+        if task_type == 'GRUPAL' and not members:
+            raise forms.ValidationError("Las tareas grupales deben tener miembros asignados.")
+        return cleaned_data
