e5d67700fb4c05683a3646b83258bf41144fe654
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-19 11:43:57 +0100

    Ajuste de la visualización de tareas en el admin

--- classroom_tasks/tasks/admin.py
+++ classroom_tasks/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    # Columnas visibles en el listado de tareas
+    list_display = ("title", "type", "status", "creator", "requires_teacher_validation", "due_date", "created_at")
+    list_filter = ("type", "status", "requires_teacher_validation")
+    search_fields = ("title", "description")
+
+    # Para ver los miembros en detalle
+    filter_horizontal = ("members",)
