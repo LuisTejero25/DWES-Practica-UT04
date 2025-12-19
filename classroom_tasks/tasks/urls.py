54c44f202cad6f2677bab55f37d62e99f05f0261
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-19 11:47:28 +0100

    Se define rutas específicas para la app tasks

---
+++ classroom_tasks/tasks/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = "tasks"
+
+urlpatterns = [
+    path('create/', views.create_task, name='create_task'),
+    path('mine/', views.list_my_tasks, name='list_my_tasks'),
+    path('to-validate/', views.tasks_to_validate, name='tasks_to_validate'),
+    path('<int:task_id>/deliver/', views.deliver_task, name='deliver_task'),
+    path('<int:task_id>/validate/', views.validate_task, name='validate_task'),
+    path('<int:task_id>/', views.task_detail, name='detail'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
+
