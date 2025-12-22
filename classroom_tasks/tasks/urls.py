dd4d0035dfe82af1e847a34b830a080bffbbb2cd
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-22 21:30:27 +0100

    Rutas de tasks reordenadas

--- classroom_tasks/tasks/urls.py
+++ classroom_tasks/tasks/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path('mine/', views.list_my_tasks, name='list_my_tasks'),
     path('to-validate/', views.tasks_to_validate, name='tasks_to_validate'),
     path('<int:task_id>/deliver/', views.deliver_task, name='deliver_task'),
-    path('<int:task_id>/validate/', views.validate_task, name='validate_task'),
-    path('<int:task_id>/', views.task_detail, name='detail'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('<int:task_id>/validate/', views.validate_task, name='validate_task'),   
     path('<int:task_id>/cancel/', views.cancel_delivery, name='cancel_delivery'),
-
-
+    path("validate-by-student/<int:task_id>/", views.validate_task_by_student, name="validate_by_student"),
+    path('<int:task_id>/', views.task_detail, name='detail'),
 ]
 
