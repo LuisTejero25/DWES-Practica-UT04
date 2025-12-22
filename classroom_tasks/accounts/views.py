64c3433e7593ef2abd062fb48bbec24273f50041
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-23 00:13:11 +0100

    refactor: actualizar lógica de vistas de usuarios

--- classroom_tasks/accounts/views.py
+++ classroom_tasks/accounts/views.py
@@ -2,12 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from accounts.models import User
 
-
+# Vista del perfil del usuario
 @login_required
 def profile_view(request):
     return render(request, "accounts/profile.html", {"user": request.user})
 
-
+# Vista con el listado de todos los usuarios
 @login_required
 def list_users_view(request):
     users = User.objects.all().order_by("username")
